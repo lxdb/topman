25878b16421e8251a396931d2a51e23a9674c240
Denys Konovalov <linuxdb.service@gmail.com>
2019-08-22 21:10:33 +0200

    Fix

--- src/file.sh
+++ src/file.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 if [[ ! -f "/usr/local/share/topman/database.db" ]]; then
     pkexec chmod -R 777 /usr/local/share/topman/
-    sqlite3 /usr/local/share/topman/database.db <<EOF
-    create table Devices  (devId INTEGER, devBrand TEXT,devModel TEXT,devOS TEXT,devHost TEXT,devState INTEGER);
-    create table Users (userId INTEGER PRIMARY KEY,userName TEXT,userSurename TEXT,userClass TEXT);
-    create table Gets (getId INTEGER PRIMARY KEY,getUser TEXT,getUserId INTEGER,getDevId INTEGER,getDate TEXT);
-EOF
 fi
 
